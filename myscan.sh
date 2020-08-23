ef1e0b54f5e4dcb0fe525ee4aea069dd8d37ed1a
Brandon Mommsen <bmmommsen@alaska.edu>
2020-08-23 13:12:25 -0800

    first

---
+++ myscan.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# get host argument from user and store in var host
+while getopts ":h:" opt; do
+  case ${opt} in
+    h )
+      host=$OPTARG
+      ;;
+    \? )
+      echo "Usage: myscan -h <target host>"; exit
+      ;;
+    : )
+      echo "Invalid usage: missing host argument"; exit
+      ;;
+  esac
+done
+
+# no args provided
+if (( $OPTIND == 1 )); then
+   echo "Usage: myscan -h <target host>"; exit
+fi
+
+# if scans directory DNE, create
+scansdir="${host}_scans"
+[ ! -d "./$scansdir" ]
+if (($? == 0)); then
+  mkdir -v $scansdir
+  if (($? == 1)); then
+    echo "Unable to make directory $scansdir due to lack of write permissions in current directory."
+    exit
+  fi
+fi
+
+
+# start scanning
+echo "scanning target $host"
+
+# top 100 scan tcp
+nmap -oX "${scansdir}/tcp_top10_scan" -T4 --top-ports100 -Pn -A $host &
+wait
+echo "wrote scan results to \"${scansdir}/tcp_top10_scan\""
+
+# top 100 scan udp
+# all ports tcp
+# all ports udp
